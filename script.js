7e97d36e43d3b855e06e035ea7d9d44e45838438
Cristofer Luchtenberg <cristof3r.dk@gmail.com>
2023-10-03 10:58:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ document.getElementById("nomeFilme").addEventListener("keypress", function(event
 });
 
 function getMovieName(){
-    if(document.querySelector("#nomeFilme").value != ""){      
-        getData()
+    if(document.querySelector("#nomeFilme").value != ""){   
+        getData() 
     } else {
         alert("Insira um filme!");
     }
@@ -23,168 +23,176 @@ async function getData() {
     const results = await fetch(`https://api.themoviedb.org/3/search/movie?query=${movieName}&include_adult=false&language=pt-BR&page=1&region=BR`, options)
         .then(response => response.json())
         if(results.results != ""){
-            results.results.map((movie) => {
-                if(movie.overview != "" && movie.poster_path != null && movie.vote_average != 0){                  
-                    loadMovies(movie);
-                }              
-            })
+            loadMovies(results.results)
         } else {
             alert("Nenhum filme encontrado!");
         }   
 }
 
-async function loadMovies(movie){
+async function loadMovies(movies){
     
-    const movies = document.querySelector(".filmes");
+    const movies_list = document.querySelector(".filmes");
 
-        const poster = document.createElement('img');
-        poster.setAttribute('class', 'poster-img');
-        poster.setAttribute('loading', 'lazy');
-        poster.setAttribute('alt', movie.title);
-        poster.setAttribute('decoding', 'async');
-        poster.setAttribute('fetchpriority', 'high');
-        poster.setAttribute('src', 'https://image.tmdb.org/t/p/w342/' + movie.poster_path)
-        poster.setAttribute('height', 230); 
+    const moviePromises = movies.map(async (movie) => {
+        if(movie.poster_path != null){
+            const poster = document.createElement('img');
+            poster.setAttribute('class', 'poster-img');
+            poster.setAttribute('loading', 'lazy');
+            poster.setAttribute('alt', movie.title);
+            poster.setAttribute('decoding', 'async');
+            poster.setAttribute('fetchpriority', 'high');
+            poster.setAttribute('src', 'https://image.tmdb.org/t/p/w342/' + movie.poster_path)
+            poster.setAttribute('height', 230); 
 
-        const div_poster = document.createElement('div');
-        div_poster.setAttribute('class', 'div-img-poster');
-        div_poster.append(poster);
+            const div_poster = document.createElement('div');
+            div_poster.setAttribute('class', 'div-img-poster');
+            div_poster.append(poster);
+            
+            const title = document.createElement('h2');
+            title.setAttribute('class', 'title');
+            title.textContent = movie.title;
+
+            const div_title = document.createElement('div-title');
+            div_title.setAttribute('class', 'div-title');
+            div_title.append(title);
+
+            const span_year = document.createElement('span')
+            span_year.setAttribute('class', 'year');
+            span_year.textContent = movie.release_date.substr(0, 4);
+            
+            const year = document.createElement('p');
+            year.setAttribute('class', 'sub');
+            year.textContent = "Ano: ";
+            year.append(span_year);
+
+            const div_year = document.createElement('div');
+            div_year.setAttribute('class', 'div-year');
+            div_year.append(year);
+
+            const span_gender = document.createElement('span');
+            span_gender.setAttribute('class', 'gender');
+            
+            getGender(movie.genre_ids).then(genreList => {
+                span_gender.textContent = genreList;
+            });
+
+            const gender = document.createElement('p');
+            gender.setAttribute('class', 'sub')
+            gender.textContent = "Gênero: ";
+            gender.append(span_gender);
+
+            const div_gender = document.createElement('div');
+            div_gender.setAttribute('class', 'div-gender')
+            div_gender.append(gender);
+
+            const div_info = document.createElement('div-info');
+            div_info.setAttribute('class', 'div-info')
+            div_info.append(div_year);
+            div_info.append(div_gender);
+
+            const rating = document.createElement('p');
+            rating.setAttribute('class', 'rating');
+            rating.textContent = movie.vote_average.toFixed(1);
+
+            const star_icon = document.createElement('img');
+            star_icon.setAttribute('src', './img/star-icon.svg');
+            star_icon.setAttribute('height', 15); 
+            star_icon.setAttribute('width', 15); 
+
+            const div_rating = document.createElement('div');
+            div_rating.setAttribute('class', 'div-rating');
+            div_rating.append(star_icon);
+            div_rating.append(rating);
+
+            const title_sinopse = document.createElement('p');
+            title_sinopse.setAttribute('class', 'sub');
+            title_sinopse.textContent = "Sinopse: ";
+
+            const info_sinopse = document.createElement('div');
+            info_sinopse.setAttribute('class', 'info-sinopse');
+            info_sinopse.append(title_sinopse);
+            info_sinopse.append(div_rating);
+
+            const sinopse_text = document.createElement('span');
+            sinopse_text.setAttribute('class', 'sinopse-text')
+            sinopse_text.textContent = movie.overview
+
+            const sinopse = document.createElement('div');
+            sinopse.setAttribute('class', 'sinopse');
+            sinopse.appendChild(sinopse_text);
+
+            const div_sinopse = document.createElement('div');
+            div_sinopse.setAttribute('class', 'div-sinopse')
+            div_sinopse.append(info_sinopse);
+            div_sinopse.append(sinopse);
         
-        const title = document.createElement('h2');
-        title.setAttribute('class', 'title');
-        title.textContent = movie.title;
+            const actor = document.createElement('span');
+            getActors(movie.id).then(actors => {
+                actors.map((url) => {
+                    const actor_link = document.createElement('a');
+                    actor_link.setAttribute('class', 'actor-link');
+                    actor_link.setAttribute('href', 'https://www.imdb.com/search/name/?name=' + url);
+                    actor_link.setAttribute('target', '_blank');
+                    actor_link.textContent = url;
+                    actor.append(actor_link);
+                })            
+            })
+            
+            const div_actors = document.createElement('div');
+            div_actors.setAttribute('class', 'div-actors')
+            div_actors.append(actor);
 
-        const div_title = document.createElement('div-title');
-        div_title.setAttribute('class', 'div-title');
-        div_title.append(title);
+            const title_cast = document.createElement('p');
+            title_cast.setAttribute('class', 'sub');
+            title_cast.textContent = 'Elenco: ';
 
-        const span_year = document.createElement('span')
-        span_year.setAttribute('class', 'year');
-        span_year.textContent = movie.release_date.substr(0, 4);
-        
-        const year = document.createElement('p');
-        year.setAttribute('class', 'sub');
-        year.textContent = "Ano: ";
-        year.append(span_year);
+            const div_cast = document.createElement('div');
+            div_cast.setAttribute('class', 'div-cast');
+            div_cast.append(title_cast);
+            div_cast.append(div_actors);
+            
+            const title_streamings = document.createElement('p');
+            title_streamings.setAttribute('class', 'sub');
+            title_streamings.textContent = 'Onde Assistir: ';
 
-        const div_year = document.createElement('div');
-        div_year.setAttribute('class', 'div-year');
-        div_year.append(year);
+            const streamings = document.createElement('div');
+            streamings.setAttribute('class', 'streamings');
+            getStreaming(movie.id, movie.title).then(streaming => {        
+                if(streaming != undefined){               
+                    streaming.map((item) => {
+                        streamings.innerHTML += item.innerHTML;                 
+                    })                
+                } else {
+                    streamings.textContent = "Não está disponível para streaming!"
+                }           
+            })
+            
+            const div_streamings = document.createElement('div');
+            div_streamings.setAttribute('class', 'div-streamings')
+            div_streamings.append(title_streamings);
+            div_streamings.append(streamings);
 
-        const span_gender = document.createElement('span');
-        span_gender.setAttribute('class', 'gender');
-        
-        getGender(movie.genre_ids).then(genreList => {
-            span_gender.textContent = genreList;
-        });
+            const filme = document.createElement('div');
+            filme.setAttribute('class', 'filme');
+            filme.setAttribute('id', movie.id);
+            filme.append(div_poster);
+            filme.append(div_title);
+            filme.append(div_info);
+            filme.append(div_sinopse);
+            filme.append(div_cast);
+            filme.append(div_streamings);
 
-        const gender = document.createElement('p');
-        gender.setAttribute('class', 'sub')
-        gender.textContent = "Gênero: ";
-        gender.append(span_gender);
-
-        const div_gender = document.createElement('div');
-        div_gender.setAttribute('class', 'div-gender')
-        div_gender.append(gender);
-
-        const div_info = document.createElement('div-info');
-        div_info.setAttribute('class', 'div-info')
-        div_info.append(div_year);
-        div_info.append(div_gender);
-
-        const rating = document.createElement('p');
-        rating.setAttribute('class', 'rating');
-        rating.textContent = movie.vote_average.toFixed(1);
-
-        const star_icon = document.createElement('img');
-        star_icon.setAttribute('src', './img/star-icon.svg');
-        star_icon.setAttribute('height', 15); 
-        star_icon.setAttribute('width', 15); 
-
-        const div_rating = document.createElement('div');
-        div_rating.setAttribute('class', 'div-rating');
-        div_rating.append(star_icon);
-        div_rating.append(rating);
-
-        const title_sinopse = document.createElement('p');
-        title_sinopse.setAttribute('class', 'sub');
-        title_sinopse.textContent = "Sinopse: ";
-
-        const info_sinopse = document.createElement('div');
-        info_sinopse.setAttribute('class', 'info-sinopse');
-        info_sinopse.append(title_sinopse);
-        info_sinopse.append(div_rating);
-
-        const sinopse_text = document.createElement('span');
-        sinopse_text.setAttribute('class', 'sinopse-text')
-        sinopse_text.textContent = movie.overview
-
-        const sinopse = document.createElement('div');
-        sinopse.setAttribute('class', 'sinopse');
-        sinopse.appendChild(sinopse_text);
-
-        const div_sinopse = document.createElement('div');
-        div_sinopse.setAttribute('class', 'div-sinopse')
-        div_sinopse.append(info_sinopse);
-        div_sinopse.append(sinopse);
-     
-        const actor = document.createElement('span');
-        getActors(movie.id).then(actors => {
-            actors.map((url) => {
-                const actor_link = document.createElement('a');
-                actor_link.setAttribute('class', 'actor-link');
-                actor_link.setAttribute('href', 'https://www.imdb.com/search/name/?name=' + url);
-                actor_link.setAttribute('target', '_blank');
-                actor_link.textContent = url;
-                actor.append(actor_link);
-            })            
-        })
-        
-        const div_actors = document.createElement('div');
-        div_actors.setAttribute('class', 'div-actors')
-        div_actors.append(actor);
-
-        const title_cast = document.createElement('p');
-        title_cast.setAttribute('class', 'sub');
-        title_cast.textContent = 'Elenco: ';
-
-        const div_cast = document.createElement('div');
-        div_cast.setAttribute('class', 'div-cast');
-        div_cast.append(title_cast);
-        div_cast.append(div_actors);
-        
-        const title_streamings = document.createElement('p');
-        title_streamings.setAttribute('class', 'sub');
-        title_streamings.textContent = 'Onde Assistir: ';
-
-        const streamings = document.createElement('div');
-        streamings.setAttribute('class', 'streamings');
-        getStreaming(movie.id, movie.title).then(streaming => {        
-            if(streaming != undefined){               
-                streaming.map((item) => {
-                    streamings.innerHTML += item.innerHTML;                 
-                })                
-            } else {
-                streamings.textContent = "Não está disponível para streaming!"
-            }           
-        })
-        
-        const div_streamings = document.createElement('div');
-        div_streamings.setAttribute('class', 'div-streamings')
-        div_streamings.append(title_streamings);
-        div_streamings.append(streamings);
-
-        const filme = document.createElement('div');
-        filme.setAttribute('class', 'filme');
-        filme.setAttribute('id', movie.id);
-        filme.append(div_poster);
-        filme.append(div_title);
-        filme.append(div_info);
-        filme.append(div_sinopse);
-        filme.append(div_cast);
-        filme.append(div_streamings);
-
-        movies.append(filme)
+            movies_list.append(filme)
+        }
+    })
+    // verifica se tudo foi carregado antes de exibir
+    try {
+        await Promise.all(moviePromises);
+        document.querySelector('body').style = "justify-content: flex-start";
+        document.querySelector('.filmes').style = "display: grid";
+    } catch (error) {
+        console.error('Erro ao carregar filmes:', error);
+    } 
 }
 
 async function getStreaming(movieId, title){
